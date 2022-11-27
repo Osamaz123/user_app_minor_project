3ba253e3b4ed1fe5aa28e2b33dfdfc64223de70b
shakir <shahahmed8286@gmail.com>
2022-11-27 21:47:56 +0530

    Fetching data from firebase problem faced while running the app because of bottom navigation

---
+++ app/src/main/java/com/example/zhcetapp/ui/notice/NoticeAdapter.java
@@ -0,0 +1,147 @@
+package com.example.zhcetapp.ui.notice;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*import com.example.collegemanagementapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;*/
+import com.example.zhcetapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeViewAdapter> {
+
+    private Context context;
+    private ArrayList<NoticeData> list;
+
+    public NoticeAdapter(Context context, ArrayList<NoticeData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public NoticeViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.news_feed_item_layout, parent, false);
+        return new NoticeViewAdapter(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoticeViewAdapter holder, @SuppressLint("RecyclerView") int position) {
+        NoticeData currentItem = list.get(position);
+        holder.userViewNoticeTitle.setText(currentItem.getTitle());
+        holder.date.setText(currentItem.getDate());
+        holder.time.setText(currentItem.getTime());
+
+        try {
+            if (currentItem.getImage() != null)
+                Picasso.get().load(currentItem.getImage()).into(holder.userViewNoticeImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class NoticeViewAdapter extends RecyclerView.ViewHolder {
+        private TextView userViewNoticeTitle, date, time;
+        private ImageView userViewNoticeImage;
+
+        public NoticeViewAdapter(@NonNull View itemView) {
+            super(itemView);
+
+            userViewNoticeTitle = itemView.findViewById(R.id.userViewNoticeTitle);
+            userViewNoticeImage = itemView.findViewById(R.id.userViewNoticeImage);
+            date = itemView.findViewById(R.id.date);
+            time = itemView.findViewById(R.id.time);
+
+        }
+    }
+//    private Context context;
+//    private ArrayList<NoticeData> list;
+//
+//    public NoticeAdapter(Context context, ArrayList<NoticeData> list) {
+//        this.context = context;
+//        this.list = list;
+//    }
+//
+//    @NonNull
+//    @Override
+//    public NoticeViewAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//       View view = LayoutInflater.from(context).inflate(R.layout.newsfeed_item_layout,parent,false);
+//        return new NoticeViewAdapter(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull NoticeViewAdapter holder, int position) {
+//
+//        NoticeData currentItem = list.get(position);
+//        holder.deleteNoticeTitle.setText(currentItem.getTitle());
+//
+//        try {
+//            if(currentItem.getImage() !=null)
+//            Picasso.get().load(currentItem.getImage()).into(holder.deleteNoticeImage);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        holder.deleteNotice.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Notice");
+//                reference.child(currentItem.getKey()).removeValue()
+//                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show();
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(context, "Something went wrong", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//               notifyItemRemoved(position);
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//
+//        return list.size();
+//    }
+//
+//    public class NoticeViewAdapter extends RecyclerView.ViewHolder {
+//                   private Button deleteNotice;
+//                    private TextView deleteNoticeTitle;
+//                     private ImageView deleteNoticeImage;
+//        public NoticeViewAdapter(@NonNull View itemView) {
+//
+//            super(itemView);
+//            deleteNotice = itemView.findViewById(R.id.deleteNotice);
+//            deleteNoticeTitle= itemView.findViewById(R.id.deleteNoticeTitle);
+//            deleteNoticeImage = itemView.findViewById(R.id.deleteNoticeImage);
+//        }
+//    }
+}
